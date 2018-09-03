523e3b6eade3f1be158181f37e6d65ec53136dc7
Dmitry Shinelev <d.s.shinelev@gmail.com>
2018-09-03 16:53:30 +0300

    plain project

---
+++ main.c
@@ -0,0 +1,386 @@
+/*
+ * VisitCard-Firmware_v3.c
+ *
+ * Created: 03.09.2018 16:32:05
+ * Author : dmitry_shinelev
+ */
+ 
+// please see http://www.frank-zhao.com/card/
+// note, for ATtiny MCUs, fuses -U lfuse:w:0xE1:m -U hfuse:w:0xDF:m -U efuse:w:0xFF:m -U lock:w:0xFF:m
+// note, write to ATtiny at a low ISP frequency
+
+// required avr-libc modules, see http://www.nongnu.org/avr-libc/user-manual/modules.html
+#include <avr/io.h> // allows access to AVR hardware registers
+#include <avr/interrupt.h> // allows enabling/disabling and declaring interrupts
+#include <util/delay.h> // includes delay functions
+#include <avr/wdt.h> // allows enabling/disabling watchdog timer
+#include <avr/pgmspace.h> // descriptor must be stored in flash memory
+#include <avr/eeprom.h> // text file and calibration data is stored in EEPROM
+#include <stdio.h> // allows streaming strings
+
+// configure settings for V-USB then include the V-USB driver so V-USB uses your settings
+#include "usbconfig.h"
+#include "usbdrv.h"
+
+// USB HID report descriptor for boot protocol keyboard
+// see HID1_11.pdf appendix B section 1
+// USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH is defined in usbconfig
+PROGMEM const char usbHidReportDescriptor[USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH] = {
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x06,                    // USAGE (Keyboard)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x95, 0x08,                    //   REPORT_COUNT (8)
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)(Key Codes)
+    0x19, 0xe0,                    //   USAGE_MINIMUM (Keyboard LeftControl)(224)
+    0x29, 0xe7,                    //   USAGE_MAXIMUM (Keyboard Right GUI)(231)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs) ; Modifier byte
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x81, 0x03,                    //   INPUT (Cnst,Var,Abs) ; Reserved byte
+    0x95, 0x05,                    //   REPORT_COUNT (5)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x05, 0x08,                    //   USAGE_PAGE (LEDs)
+    0x19, 0x01,                    //   USAGE_MINIMUM (Num Lock)
+    0x29, 0x05,                    //   USAGE_MAXIMUM (Kana)
+    0x91, 0x02,                    //   OUTPUT (Data,Var,Abs) ; LED report
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x75, 0x03,                    //   REPORT_SIZE (3)
+    0x91, 0x03,                    //   OUTPUT (Cnst,Var,Abs) ; LED report padding
+    0x95, 0x06,                    //   REPORT_COUNT (6)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x65,                    //   LOGICAL_MAXIMUM (101)
+    0x05, 0x07,                    //   USAGE_PAGE (Keyboard)(Key Codes)
+    0x19, 0x00,                    //   USAGE_MINIMUM (Reserved (no event indicated))(0)
+    0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)(101)
+    0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
+    0xc0                           // END_COLLECTION
+};
+
+// data structure for boot protocol keyboard report
+// see HID1_11.pdf appendix B section 1
+typedef struct {
+	uint8_t modifier;
+	uint8_t reserved;
+	uint8_t keycode[6];
+} keyboard_report_t;
+
+// global variables
+
+static keyboard_report_t keyboard_report;
+#define keyboard_report_reset() keyboard_report.modifier=0;keyboard_report.reserved=0;keyboard_report.keycode[0]=0;keyboard_report.keycode[1]=0;keyboard_report.keycode[2]=0;keyboard_report.keycode[3]=0;keyboard_report.keycode[4]=0;keyboard_report.keycode[5]=0;
+static uint8_t idle_rate = 500 / 4; // see HID1_11.pdf sect 7.2.4
+static uint8_t protocol_version = 0; // see HID1_11.pdf sect 7.2.6
+static uint8_t LED_state = 0; // see HID1_11.pdf appendix B section 1
+static uint8_t blink_count = 0; // keep track of how many times caps lock have toggled
+
+// see http://vusb.wikidot.com/driver-api
+// constants are found in usbdrv.h
+usbMsgLen_t usbFunctionSetup(uint8_t data[8])
+{
+	// see HID1_11.pdf sect 7.2 and http://vusb.wikidot.com/driver-api
+	usbRequest_t *rq = (void *)data;
+
+	if ((rq->bmRequestType & USBRQ_TYPE_MASK) != USBRQ_TYPE_CLASS)
+		return 0; // ignore request if it's not a class specific request
+
+	// see HID1_11.pdf sect 7.2
+	switch (rq->bRequest)
+	{
+		case USBRQ_HID_GET_IDLE:
+			usbMsgPtr = &idle_rate; // send data starting from this byte
+			return 1; // send 1 byte
+		case USBRQ_HID_SET_IDLE:
+			idle_rate = rq->wValue.bytes[1]; // read in idle rate
+			return 0; // send nothing
+		case USBRQ_HID_GET_PROTOCOL:
+			usbMsgPtr = &protocol_version; // send data starting from this byte
+			return 1; // send 1 byte
+		case USBRQ_HID_SET_PROTOCOL:
+			protocol_version = rq->wValue.bytes[1];
+			return 0; // send nothing
+		case USBRQ_HID_GET_REPORT:
+			usbMsgPtr = &keyboard_report; // send the report data
+			return sizeof(keyboard_report);
+		case USBRQ_HID_SET_REPORT:
+			if (rq->wLength.word == 1) // check data is available
+			{
+				// 1 byte, we don't check report type (it can only be output or feature)
+				// we never implemented "feature" reports so it can't be feature
+				// so assume "output" reports
+				// this means set LED status
+				// since it's the only one in the descriptor
+				return USB_NO_MSG; // send nothing but call usbFunctionWrite
+			}
+			else // no data or do not understand data, ignore
+			{
+				return 0; // send nothing
+			}
+		default: // do not understand data, ignore
+			return 0; // send nothing
+	}
+}
+
+// see http://vusb.wikidot.com/driver-api
+usbMsgLen_t usbFunctionWrite(uint8_t * data, uchar len)
+{
+	if (data[0] != LED_state)
+	{
+		// increment count when LED has toggled
+		blink_count = blink_count < 10 ? blink_count + 1 : blink_count;
+	}
+	
+	LED_state = data[0];
+	
+	#if !defined(__AVR_ATtiny85__) && !defined(__AVR_ATtiny45__) && !defined(__AVR_ATtiny25__)
+	 // LED lights for debug
+	if (bit_is_set(LED_state, 1))
+	{
+		DDRD |= _BV(0);
+		PORTD |= _BV(0);
+	}
+	else
+	{
+		DDRD |= _BV(0);
+		PORTD &= ~_BV(0);
+	}
+	#endif
+	
+	return 1; // 1 byte read
+}
+
+// translates ASCII to appropriate keyboard report, taking into consideration the status of caps lock
+void ASCII_to_keycode(uint8_t ascii)
+{
+	keyboard_report.keycode[0] = 0x00;
+	keyboard_report.modifier = 0x00;
+	
+	// see scancode.doc appendix C
+	
+	if (ascii >= 'A' && ascii <= 'Z')
+	{
+		keyboard_report.keycode[0] = 4 + ascii - 'A'; // set letter
+		if (bit_is_set(LED_state, 1)) // if caps is on
+		{
+			keyboard_report.modifier = 0x00; // no shift
+		}
+		else
+		{
+			keyboard_report.modifier = _BV(1); // hold shift // hold shift
+		}
+	}
+	else if (ascii >= 'a' && ascii <= 'z')
+	{
+		keyboard_report.keycode[0] = 4 + ascii - 'a'; // set letter
+		if (bit_is_set(LED_state, 1)) // if caps is on
+		{
+			keyboard_report.modifier = _BV(1); // hold shift // hold shift
+		}
+		else
+		{
+			keyboard_report.modifier = 0x00; // no shift
+		}
+	}
+	else if (ascii >= '0' && ascii <= '9')
+	{
+		keyboard_report.modifier = 0x00;
+		if (ascii == '0')
+		{
+			keyboard_report.keycode[0] = 0x27;
+		}
+		else
+		{
+			keyboard_report.keycode[0] = 30 + ascii - '1'; 
+		}
+	}
+	else
+	{
+		switch (ascii) // convert ascii to keycode according to documentation
+		{
+			case '!':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 29 + 1;
+				break;
+			case '@':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 29 + 2;
+				break;
+			case '#':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 29 + 3;
+				break;
+			case '$':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 29 + 4;
+				break;
+			case '%':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 29 + 5;
+				break;
+			case '^':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 29 + 6;
+				break;
+			case '&':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 29 + 7;
+				break;
+			case '*':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 29 + 8;
+				break;
+			case '(':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 29 + 9;
+				break;
+			case ')':
+				keyboard_report.modifier = _BV(1); // hold shift
+				keyboard_report.keycode[0] = 0x27;
+				break;
+			case '~':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case '`':
+				keyboard_report.keycode[0] = 0x35;
+				break;
+			case '_':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case '-':
+				keyboard_report.keycode[0] = 0x2D;
+				break;
+			case '+':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case '=':
+				keyboard_report.keycode[0] = 0x2E;
+				break;
+			case '{':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case '[':
+				keyboard_report.keycode[0] = 0x2F;
+				break;
+			case '}':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case ']':
+				keyboard_report.keycode[0] = 0x30;
+				break;
+			case '|':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case '\\':
+				keyboard_report.keycode[0] = 0x31;
+				break;
+			case ':':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case ';':
+				keyboard_report.keycode[0] = 0x33;
+				break;
+			case '"':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case '\'':
+				keyboard_report.keycode[0] = 0x34;
+				break;
+			case '<':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case ',':
+				keyboard_report.keycode[0] = 0x36;
+				break;
+			case '>':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case '.':
+				keyboard_report.keycode[0] = 0x37;
+				break;
+			case '?':
+				keyboard_report.modifier = _BV(1); // hold shift
+				// fall through
+			case '/':
+				keyboard_report.keycode[0] = 0x38;
+				break;
+			case ' ':
+				keyboard_report.keycode[0] = 0x2C;
+				break;
+			case '\t':
+				keyboard_report.keycode[0] = 0x2B;
+				break;
+			case '\n':
+				keyboard_report.keycode[0] = 0x28;
+				break;
+		}
+	}
+}
+
+void send_report_once()
+{
+	// perform usb background tasks until the report can be sent, then send it
+	while (1)
+	{
+		usbPoll(); // this needs to be called at least once every 10 ms
+		if (usbInterruptIsReady())
+		{
+			usbSetInterrupt(&keyboard_report, sizeof(keyboard_report)); // send
+			break;
+			
+			// see http://vusb.wikidot.com/driver-api
+		}
+	}
+}
+
+// stdio's stream will use this funct to type out characters in a string
+void type_out_char(uint8_t ascii, FILE *stream)
+{
+	ASCII_to_keycode(ascii);
+	send_report_once();
+	keyboard_report_reset(); // release keys
+	send_report_once();
+}
+
+static FILE mystdout = FDEV_SETUP_STREAM(type_out_char, NULL, _FDEV_SETUP_WRITE); // setup writing stream
+
+int main()
+{	
+	stdout = &mystdout; // set default stream
+	
+	// initialize report (I never assume it's initialized to 0 automatically)
+	keyboard_report_reset();
+	
+	wdt_disable(); // disable watchdog, good habit if you don't use it
+	
+	// enforce USB re-enumeration by pretending to disconnect and reconnect
+	usbDeviceDisconnect();
+	_delay_ms(250);
+	usbDeviceConnect();
+	
+	// initialize various modules
+	usbInit();
+	
+	sei(); // enable interrupts
+	
+	while (1) // main loop, do forever
+	{
+		if (blink_count > 2) // activated by blinking lights
+		{
+			
+			// PLACE TEXT HERE
+			//puts_P(PSTR(" ")); // test size
+			puts_P(PSTR("RMCSoft LLC\nCharlotte, NC\n933 Louise Ave., Suite 101S, Charlotte, NC 28204, USA\n(980) 201-2460\n\nNick Gritsenko\nnick@rmcsoft.com\n\nOlga Muller\nolga.z.muller@gmail.com"));
+			
+			blink_count = 0; // reset
+		}
+		
+		// perform usb related background tasks
+		usbPoll(); // this needs to be called at least once every 10 ms
+		// this is also called in send_report_once
+	}
+	
+	return 0;
+}
